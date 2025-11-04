3689d7fea0c65fd606938dce33c6f49a7c6db955
Midnight Coding <midnightcoding.dev@gmail.com>
2025-11-03 21:37:53 -0300

    � Add Short #002 — Variables in Java

---
+++ shorts/02-variables-java/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        int age = 25;
+        String name = "Midnight Coder";
+        double focusHours = 1.5;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Focus Hours: " + focusHours + "h.");
+    }
+}
